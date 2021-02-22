550a55d37ef4eae8dd2fad837103f98250a2dfbc
thomaslouisgibson <thomaslouisgibson@gmail.com>
2021-02-22 23:39:44 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,3 +18,15 @@ resource "aws_dynamodb_table" "tfc_example_table" {
     type = "S"
   }
 }
+
+
+resource "aws_elastic_beanstalk_application" "tftest" {
+  name        = "tf-test-name"
+  description = "tf-test-desc"
+
+  appversion_lifecycle {
+    service_role          = aws_iam_role.beanstalk_service.arn
+    max_count             = 128
+    delete_source_from_s3 = true
+  }
+}
