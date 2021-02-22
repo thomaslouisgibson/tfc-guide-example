c9e770a0b2484ef2b9de96c0d6c33568398a4f62
thomaslouisgibson <thomaslouisgibson@gmail.com>
2021-02-22 23:47:45 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,19 +6,6 @@ provider "random" {}
 
 resource "random_pet" "table_name" {}
 
-resource "aws_dynamodb_table" "tfc_example_table" {
-  name = "${var.db_table_name}-${random_pet.table_name.id}"
-
-  read_capacity  = var.db_read_capacity
-  write_capacity = var.db_write_capacity
-  hash_key       = "UUID"
-
-  attribute {
-    name = "UUID"
-    type = "S"
-  }
-}
-
 
 resource "aws_iam_service_linked_role" "elasticbeanstalk" {
   aws_service_name = "elasticbeanstalk.amazonaws.com"
